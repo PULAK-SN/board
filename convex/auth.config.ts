10bb5ff6a6db970b15403dda5d24a31c6ecf656f
PULAK-SN <pulak.official22@gmail.com>
2025-06-12 22:44:06 +0530

    seting up convex and clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+export default {
+  providers: [
+    {
+      domain: "https://literate-roughy-6.clerk.accounts.dev",
+      //   domain: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_URL,
+      applicationID: "convex",
+    },
+  ],
+};
