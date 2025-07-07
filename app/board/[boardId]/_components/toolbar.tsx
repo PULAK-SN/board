ec7ef1aa96152bc165f2e89ed6f23d9366b28edb
PULAK-SN <pulak.official22@gmail.com>
2025-07-08 00:40:07 +0530

    toolbar icon setup

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,3 +1,15 @@
+import {
+  Circle,
+  MousePointer2,
+  Pencil,
+  Redo,
+  Square,
+  StickyNote,
+  Type,
+  Undo,
+} from "lucide-react";
+import { ToolButton } from "./tool-button";
+
 export const Toolbar = () => {
   return (
     <div
@@ -8,15 +20,59 @@ export const Toolbar = () => {
         className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col
         items-center shadow-md"
       >
-        <div>Pencil</div>
-        <div>Circle</div>
-        <div>Squere</div>
-        <div>Ellipsis</div>
+        <ToolButton
+          label="Select"
+          icon={MousePointer2}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Text"
+          icon={Type}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Sticky note"
+          icon={StickyNote}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Rectangle"
+          icon={Square}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Ellipse"
+          icon={Circle}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Pen"
+          icon={Pencil}
+          onClick={() => {}}
+          isActive={false}
+        />
       </div>
 
       <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-        <div>Undo</div>
-        <div>Redo</div>
+        <ToolButton
+          label="Undo"
+          icon={Undo}
+          onClick={() => {}}
+          isActive={false}
+          isDisabled={true}
+        />
+        <ToolButton
+          label="Redo"
+          icon={Redo}
+          onClick={() => {}}
+          isActive={false}
+          isDisabled={true}
+        />
       </div>
     </div>
   );
